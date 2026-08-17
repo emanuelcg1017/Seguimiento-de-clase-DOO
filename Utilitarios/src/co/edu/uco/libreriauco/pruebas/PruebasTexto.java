d66cbfa87b3d5dc7297e14b4253452030ef09227
Emanuel Carmona Gaviria <emanuel.carmona0467@uco.net.co>
2026-08-16 20:16:04 -0500

    Clase 11/08/2026

---
+++ Utilitarios/src/co/edu/uco/libreriauco/pruebas/PruebasTexto.java
@@ -0,0 +1,22 @@
+package co.edu.uco.libreriauco.pruebas;
+
+public class PruebasTexto {
+
+	public static void main(String[] args) {
+		
+		String miVariable = null;
+		System.out.println(miVariable);
+		
+		
+		if(miVariable == null)
+		{
+			miVariable = "";
+		}
+		System.out.println(miVariable);
+		System.out.println(miVariable.length());
+		
+		
+
+	}
+
+}
